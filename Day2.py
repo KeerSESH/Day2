ff535484b09f81d14030110c3093b47692ea7423
KeerSESH <baclanidze61@gmail.com>
2024-09-04 22:47:49 +0300

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,5 @@
+total_completed_homework = 12
+number_of_hours_spent = 1.5
+course_title = "Python"
+time_for_one_task = (number_of_hours_spent / total_completed_homework)
+print ("Курс:", course_title,",""всего задач:", total_completed_homework,",""затрачено часов:", number_of_hours_spent,",""среднее время выполнения", time_for_one_task,"часа")
